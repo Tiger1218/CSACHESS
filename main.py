0925ec5fe5e51004b84d6164b1b393bb4339322f
Tiger1218 <tiger1218@foxmail.com>
2021-08-09 07:44:20 +0800

    rebuilt

--- main.py
+++ main.py
@@ -26,9 +26,10 @@ class Chessman:
         return self.side == other.side and self.name == other.name and self.pos == other.pos
 
     def move(self, tables, pos):
-        tables[self.pos[0]][self.pos[1]] = null_chessman
-        tables[pos[0]][pos[1]] = self
-        return tables
+        table = tables
+        table[self.pos[0]][self.pos[1]] = null_chessman
+        table[pos[0]][pos[1]] = self
+        return table
 
     def reachPlace(self, tables):
         reachList = []
@@ -234,73 +235,68 @@ def checkmateChecking(tables, side):
 
 class MainGame:
     def __init__(self, chess_theme, table_theme):
+        self.flips = False
         self.chess_theme = chess_theme
         self.table_theme = table_theme
         self.table = numpy.full((9, 10), null_chessman)
+        self.chessman = []
+        self.pictures = []
+        self.initTable()
+        self.flashTable()
+
+    def flashTable(self):
+        for chessman in self.chessman:
+            self.table[chessman.pos[0]][chessman.pos[1]] = chessman
 
     def initTable(self):
         # 初始化棋子和棋子的位置
-        pictureList = initPictures(self.chess_theme, self.table_theme)
-        chessmanList = [Rook("black", "black_rook_left", pictureList["black_rook"], (8, 9)),
-                        Rook("black", "black_rook_right", pictureList["black_rook"], (0, 9)),
-                        Knight("black", "black_knight_left", pictureList["black_knight"], (7, 9)),
-                        Knight("black", "black_knight_right", pictureList["black_knight"], (1, 9)),
-                        Bishop("black", "black_bishop_left", pictureList["black_bishop"], (6, 9)),
-                        Bishop("black", "black_bishop_right", pictureList["black_bishop"], (2, 9)),
-                        Guardian("black", "black_guardian_left", pictureList["black_guardian"], (5, 9)),
-                        Guardian("black", "black_guardian_right", pictureList["black_guardian"], (3, 9)),
-                        King("black", "black_king_", pictureList["black_king"], (4, 9)),
-                        Cannon("black", "black_cannon_left", pictureList["black_cannon"], (7, 7)),
-                        Cannon("black", "black_cannon_right", pictureList["black_cannon"], (1, 7)),
-                        Soldier("black", "black_soldier_1", pictureList["black_soldier"], (0, 6)),
-                        Soldier("black", "black_soldier_2", pictureList["black_soldier"], (2, 6)),
-                        Soldier("black", "black_soldier_3", pictureList["black_soldier"], (4, 6)),
-                        Soldier("black", "black_soldier_4", pictureList["black_soldier"], (6, 6)),
-                        Soldier("black", "black_soldier_5", pictureList["black_soldier"], (8, 6)),
-                        Rook("red", "red_rook_left", pictureList["red_rook"], (0, 0)),
-                        Rook("red", "red_rook_right", pictureList["red_rook"], (8, 0)),
-                        Knight("red", "red_knight_left", pictureList["red_knight"], (1, 0)),
-                        Knight("red", "red_knight_right", pictureList["red_knight"], (7, 0)),
-                        Bishop("red", "red_bishop_left", pictureList["red_bishop"], (2, 0)),
-                        Bishop("red", "red_bishop_right", pictureList["red_bishop"], (6, 0)),
-                        Guardian("red", "red_guardian_left", pictureList["red_guardian"], (3, 0)),
-                        Guardian("red", "red_guardian_right", pictureList["red_guardian"], (5, 0)),
-                        King("red", "red_king_", pictureList["red_king"], (4, 0)),
-                        Cannon("red", "red_cannon_left", pictureList["red_cannon"], (1, 2)),
-                        Cannon("red", "red_cannon_right", pictureList["red_cannon"], (7, 2)),
-                        Soldier("red", "red_soldier_1", pictureList["red_soldier"], (0, 3)),
-                        Soldier("red", "red_soldier_2", pictureList["red_soldier"], (2, 3)),
-                        Soldier("red", "red_soldier_3", pictureList["red_soldier"], (4, 3)),
-                        Soldier("red", "red_soldier_4", pictureList["red_soldier"], (6, 3)),
-                        Soldier("red", "red_soldier_5", pictureList["red_soldier"], (8, 3))]
-        for chessman in chessmanList:
-            self.table[chessman.pos[0]][chessman.pos[1]] = chessman
-        return pictureList, chessmanList
+        self.pictures = initPictures(self.chess_theme, self.table_theme)
+        self.chessman = [Rook("black", "black_rook_left", self.pictures["black_rook"], (8, 9)),
+                         Rook("black", "black_rook_right", self.pictures["black_rook"], (0, 9)),
+                         Knight("black", "black_knight_left", self.pictures["black_knight"], (7, 9)),
+                         Knight("black", "black_knight_right", self.pictures["black_knight"], (1, 9)),
+                         Bishop("black", "black_bishop_left", self.pictures["black_bishop"], (6, 9)),
+                         Bishop("black", "black_bishop_right", self.pictures["black_bishop"], (2, 9)),
+                         Guardian("black", "black_guardian_left", self.pictures["black_guardian"], (5, 9)),
+                         Guardian("black", "black_guardian_right", self.pictures["black_guardian"], (3, 9)),
+                         King("black", "black_king_", self.pictures["black_king"], (4, 9)),
+                         Cannon("black", "black_cannon_left", self.pictures["black_cannon"], (7, 7)),
+                         Cannon("black", "black_cannon_right", self.pictures["black_cannon"], (1, 7)),
+                         Soldier("black", "black_soldier_1", self.pictures["black_soldier"], (0, 6)),
+                         Soldier("black", "black_soldier_2", self.pictures["black_soldier"], (2, 6)),
+                         Soldier("black", "black_soldier_3", self.pictures["black_soldier"], (4, 6)),
+                         Soldier("black", "black_soldier_4", self.pictures["black_soldier"], (6, 6)),
+                         Soldier("black", "black_soldier_5", self.pictures["black_soldier"], (8, 6)),
+                         Rook("red", "red_rook_left", self.pictures["red_rook"], (0, 0)),
+                         Rook("red", "red_rook_right", self.pictures["red_rook"], (8, 0)),
+                         Knight("red", "red_knight_left", self.pictures["red_knight"], (1, 0)),
+                         Knight("red", "red_knight_right", self.pictures["red_knight"], (7, 0)),
+                         Bishop("red", "red_bishop_left", self.pictures["red_bishop"], (2, 0)),
+                         Bishop("red", "red_bishop_right", self.pictures["red_bishop"], (6, 0)),
+                         Guardian("red", "red_guardian_left", self.pictures["red_guardian"], (3, 0)),
+                         Guardian("red", "red_guardian_right", self.pictures["red_guardian"], (5, 0)),
+                         King("red", "red_king_", self.pictures["red_king"], (4, 0)),
+                         Cannon("red", "red_cannon_left", self.pictures["red_cannon"], (1, 2)),
+                         Cannon("red", "red_cannon_right", self.pictures["red_cannon"], (7, 2)),
+                         Soldier("red", "red_soldier_1", self.pictures["red_soldier"], (0, 3)),
+                         Soldier("red", "red_soldier_2", self.pictures["red_soldier"], (2, 3)),
+                         Soldier("red", "red_soldier_3", self.pictures["red_soldier"], (4, 3)),
+                         Soldier("red", "red_soldier_4", self.pictures["red_soldier"], (6, 3)),
+                         Soldier("red", "red_soldier_5", self.pictures["red_soldier"], (8, 3))]
 
     def showTables(self):
-        pictureList, chessmanList = self.initTable()
         pygame.init()
         size = width, height = 521 + 300, 577
         screen = pygame.display.set_mode(size=size)
-        pygame.display.set_icon(pictureList["icon"])
+        pygame.display.set_icon(self.pictures["icon"])
         pygame.display.set_caption("雅礼中学计算机协会象棋人工智能 v0.1 by tiger1218")
         circles = []
         while True:
-            xChessman = self.eventJudge()
-            # print(xChessman)
-
-            if xChessman is not None:
-                if xChessman != null_chessman:
-                    circles = []
-                    for x, y in xChessman.reachPlaceB(self.table):
-                        print(xChessman.reachPlaceB(self.table))
-                        print(CONVERT_P(x, y))
-                        # pygame.draw.circle(screen, BLUE, CONVERT_P(x, y), 4, 4)
-                        circles.append(CONVERT_P(x, y))
-                        # pygame.display.flip()
-            # time.sleep(0.1)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit(0)
             screen.fill(BLACK)
-            screen.blit(pictureList["main_table"], pictureList["main_table"].get_rect())
+            screen.blit(self.pictures["main_table"], self.pictures["main_table"].get_rect())
             for x, y in A_TABLE:
                 if self.table[x][y] != null_chessman:
                     chessman = self.table[x][y]
@@ -309,7 +305,7 @@ class MainGame:
                 pygame.draw.circle(screen, BLUE, (circle[0], circle[1]), 8, 8)
             pygame.display.flip()
 
-    def eventJudge(self):
+    def eventJudge(self, lastchessman=None):
         time.sleep(0.1)
         eventList = pygame.event.get()
         for event in eventList:
@@ -318,15 +314,31 @@ class MainGame:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 chessPos = (round((pos[0] - 36) / 57), 9 - round((pos[1] - 28) / 57))
-                # print(pos,chessPos)
-                if chessPos not in A_TABLE or self.table[chessPos[0]][chessPos[1]] == null_chessman:
-                    return null_chessman
+                print(self.flips)
+                if self.flips:
+                    if chessPos not in A_TABLE or self.table[chessPos[0]][chessPos[1]] == null_chessman:
+                        return null_chessman
+                    else:
+                        pictureList, chessmanList = self.initTable()
+                        xChessman = self.table[chessPos[0]][chessPos[1]]
+                        xChessman.display = pictureList[nameProcess(xChessman.name) + "_select"]
+                        self.table[chessPos[0]][chessPos[1]] = xChessman
+                        return xChessman
                 else:
-                    pictureList, chessmanList = self.initTable()
-                    xChessman = self.table[chessPos[0]][chessPos[1]]
-                    xChessman.display = pictureList[nameProcess(xChessman.name) + "_select"]
-                    self.table[chessPos[0]][chessPos[1]] = xChessman
-                    return xChessman
+                    if chessPos not in A_TABLE or (chessPos[0], chessPos[1]) not in lastchessman.reachPlaceB(
+                            self.table):
+                        return null_chessman
+                    else:
+                        oldTable = self.table
+                        print(chessPos, lastchessman.pos)
+                        self.table[lastchessman.pos[0]][lastchessman.pos[1]] = null_chessman
+                        print(self.table[lastchessman.pos[0]][lastchessman.pos[1]].name)
+                        print(self.table[chessPos[0]][chessPos[1]].name)
+                        newTable = self.table
+                        print(oldTable == newTable)
+                        #     print("????")
+                        self.flips = True
+
         return null_chessman
 
 
